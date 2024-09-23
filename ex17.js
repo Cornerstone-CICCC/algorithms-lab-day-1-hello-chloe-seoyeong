eb87c3ac56d5eb6b2a03c93630add18e325e7f23
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-23 12:02:22 -0700

    ex17 completed

--- ex17.js
+++ ex17.js
@@ -2,6 +2,20 @@
 
 function mostFrequent(arr) {
   // your code here
+  let countGlobal = 0;
+  let fruit = null;
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < arr.length; j++) {
+      let countLocal = 0;
+      if(i !== j && arr[i] === arr[j]) {
+        countLocal++;
+      }
+      if (countLocal > countGlobal) {
+        fruit = arr[i];
+      }
+    }
+  }
+  return fruit;
 }
 
 console.log(mostFrequent(['apple', 'banana', 'apple', 'orange', 'banana', 'apple'])) // 'apple'

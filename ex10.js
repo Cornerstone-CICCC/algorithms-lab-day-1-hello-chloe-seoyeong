a0fa51548286680f156844be614fcfd9734b988b
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-23 10:42:23 -0700

    ex10 completed

--- ex10.js
+++ ex10.js
@@ -2,6 +2,50 @@
 
 function formatDate(dateStr) {
   // your code here
+  let month;
+  let dateArray = dateStr.split('-');
+
+  switch(dateArray[2]) {
+    case "01":
+      month = "January";
+      break;
+    case "02":
+      month = "Februrary";
+      break;
+    case "03":
+      month = "March";
+      break;
+    case "04":
+      month = "April";
+      break;
+    case "05":
+      month = "May";
+      break;
+    case "06":
+      month = "June";
+      break;
+    case "07":
+      month = "July";
+      break;
+    case "08":
+      month = "August";
+      break;
+    case "09":
+      month = "September";
+      break;
+    case "10":
+      month = "October";
+      break;
+    case "11":
+      month = "November";
+      break;
+    case "12":
+      month = "December";
+      break;
+  }
+  newDateFormat = `${month} ${parseInt(dateArray[2])}, ${dateArray[0]}`;
+
+  return newDateFormat;
 }
 
 console.log(formatDate('2022-01-01')) // 'January 1, 2022'

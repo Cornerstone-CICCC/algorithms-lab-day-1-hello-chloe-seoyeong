dca9fa6973bdcfade3da088995912b6514a1f8a5
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-23 09:54:27 -0700

    ex07 completed

--- ex07.js
+++ ex07.js
@@ -2,6 +2,11 @@
 
 function calculateAverage(arr) {
   // your code here
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum/arr.length;
 }
 
 console.log(calculateAverage([10, 20, 30, 40, 50])) // 30

8479ff26b843dadb8b4370d770f3b947ea7712b6
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-23 11:44:48 -0700

    ex16 completed

--- ex16.js
+++ ex16.js
@@ -2,6 +2,13 @@
 
 function sumNestedArray(arr) {
   // your code here
+  let flatArray = arr.flat(2);
+  let sum = 0; 
+  for(let i = 0; i < flatArray.length; i++) {
+    sum += flatArray[i];
+  }
+
+  return sum;
 }
 
 console.log(sumNestedArray([[1, 2, 3], [4, 5], [6, 7, 8]])) // 36

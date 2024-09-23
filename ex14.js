60038ac25ce0d1ca9e6adc5dfc19dea23d28e9a1
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-23 11:41:26 -0700

    ex14 completed

--- ex14.js
+++ ex14.js
@@ -2,6 +2,10 @@
 
 function mergeArrays(arr1, arr2) {
   // your code here
+  let mergeArr = arr1.concat(arr2);
+  let newArray = mergeArr.sort();
+
+  return newArray
 }
 
 console.log(mergeArrays([1, 3, 5], [2, 4, 6])) // [1, 2, 3, 4, 5, 6]

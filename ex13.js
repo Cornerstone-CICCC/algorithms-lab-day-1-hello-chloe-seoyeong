edb7271e9cc913b0ca78d5cd83324ef560fd2ff4
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-23 10:59:03 -0700

    ex13 completed

--- ex13.js
+++ ex13.js
@@ -2,6 +2,12 @@
 
 function findById(arr, id) {
   // your code here
+  let findIt;
+  for(let i = 0; i < arr.length; i++) {
+    arr[i].id == id ? findIt = arr[i] : findIt = null;
+  }
+
+  return findIt;
 }
 
 console.log(findById([{ id: 1, name: 'John' }, { id: 2, name: 'Jane' }], 2)) // { id: 2, name: 'Jane' }

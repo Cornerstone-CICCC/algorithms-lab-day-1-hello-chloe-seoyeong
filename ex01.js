8925313a903ccadbe0f0dcc7985d97f8b56d6437
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-23 08:41:35 -0700

    ex01 completed

--- ex01.js
+++ ex01.js
@@ -1,7 +1,13 @@
 // Exercise 1: Write a function named 'sumPrices' that takes an array of product objects (each with a 'price' property) and returns the total sum of all prices.
 
 function sumPrices(products) {
+  let sum = null;
   // your code here
+  for(let i = 0; i < products.length; i++) {
+    sum += products[i].price;
+  }
+
+  return sum;
 }
 
 console.log(sumPrices([{ price: 10 }, { price: 20 }, { price: 30 }])) // 60

32f00527d69f349ac550ec09e87eecbea61a8f28
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-23 09:39:00 -0700

    ex04 completed

--- ex04.js
+++ ex04.js
@@ -2,6 +2,18 @@
 
 function countOccurrences(arr) {
   // your code here
+  let fruit = {};
+  
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] in fruit) {
+      let count = fruit[`${arr[i]}`] + 1;
+      fruit[`${arr[i]}`] = count;
+    } else {
+      fruit[`${arr[i]}`] = 1;
+    }
+  }
+
+  return fruit;
 }
 
 console.log(countOccurrences(['apple', 'banana', 'apple'])) // { apple: 2, banana: 1 }

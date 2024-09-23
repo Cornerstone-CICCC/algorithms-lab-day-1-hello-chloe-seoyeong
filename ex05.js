c4c91764c00f29b8a67e75e6d8212c365fd9dbe9
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-23 09:47:47 -0700

    ex05 completed

--- ex05.js
+++ ex05.js
@@ -2,6 +2,16 @@
 
 function groupByCategory(products) {
   // your code here
+  let category = {};
+  for(let i = 0; i < products.length; i++) {
+    if(products[i].category in category) {
+      category[`${products[i].category}`].push(products[i])
+    } else {
+      category[`${products[i].category}`] = [products[i]];
+    }
+  }
+  
+  return category;
 }
 
 console.log(groupByCategory([{ name: 'Apple', category: 'Fruit' }, { name: 'Carrot', category: 'Vegetable' }, { name: 'Banana', category: 'Fruit' }])) // { Fruit: [{ name: 'Apple', category: 'Fruit' }, { name: 'Banana', category: 'Fruit' }], Vegetable: [{ name: 'Carrot', category: 'Vegetable' }] }

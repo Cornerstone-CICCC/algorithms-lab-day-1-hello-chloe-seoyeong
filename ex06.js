4b877057be3765aa14c1a1c623ad435acd1fd81d
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-23 09:52:46 -0700

    ex06 completed

--- ex06.js
+++ ex06.js
@@ -2,6 +2,14 @@
 
 function uniqueValues(arr) {
   // your code here
+  let uniqueArray = [];
+  for(let i = 0; i < arr.length; i++) {
+    if (!uniqueArray.includes(arr[i])) {
+      uniqueArray.push(arr[i])
+    }
+  }
+
+  return uniqueArray;
 }
 
 console.log(uniqueValues([1, 2, 2, 3, 4, 4, 5])) // [1, 2, 3, 4, 5]
